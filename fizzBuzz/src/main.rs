6c7afdbc0c0fcf067c838f9c3ce2309824b7ebe8
miiton <468745+miiton@users.noreply.github.com>
2019-02-17 15:43:22 +0900

    fizzbuzzがおかしかったので修正

--- fizzBuzz/src/main.rs
+++ fizzBuzz/src/main.rs
@@ -1,11 +1,14 @@
 fn main() {
-    for i in 1..100 {
-        if i%3 == 0 {
+    // 100まで
+    for i in 1..101 {
+        if i%3 == 0 && i%5 == 0 {
             println!("{} {}", i, "FizzBuzz!!")
-        } else if i%2 == 0 {
+        } else if i%3 == 0 {
+            println!("{} {}", i, "Fizz")
+        } else if i%5 == 0 {
             println!("{} {}", i, "Buzz")
         } else {
-            println!("{} {}", i, "Fizz")
+            println!("{} {}", i, "...")
         }
     }
 }
